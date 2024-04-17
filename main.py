c54ee84076642582eebcc4182b254fbb70d86088
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-04-17 19:14:17 +0100

    my work

---
+++ main.py
@@ -0,0 +1,27 @@
+def pattern1(num_of_letters):
+    """
+    Goal:
+    p
+    py
+    pyt
+    pyth
+    pytho
+    python
+    """
+pattern1("python")
+print("p")
+print("py")
+print("pyt")
+print("pyth")
+print("pytho")
+print("python")
+
+Language="python"
+print(Language[0:0+1])
+print(Language[0:1+1])
+print(Language[0:2+1])
+print(Language[0:3+1])
+print(Language[0:4+1])
+print(Language[0:5+1])
+for i in range(6):
+    print(Language[0:i+1])
---
+++ test.py
@@ -0,0 +1,31 @@
+Language="united states of america"
+print(Language[0:0+1])
+print(Language[0:1+1])
+print(Language[0:2+1])
+print(Language[0:3+1])
+print(Language[0:4+1])
+print(Language[0:5+1])
+print(" ")
+print(Language[0:6+1])
+print(Language[0:7+1])
+print(Language[0:8+1])
+print(Language[0:9+1])
+print(Language[0:10+1])
+print(Language[0:11+1])
+print(" ")
+print(Language[0:12+1])
+print(Language[0:13+1])
+print(" ")
+print(Language[0:14+1])
+print(Language[0:15+1])
+print(Language[0:16+1])
+print(Language[0:17+1])
+print(Language[0:18+1])
+print(Language[0:19+1])
+print(Language[0:20+1])
+
+
+
+
+for i in range(len(Language)):
+ print(Language[0:i+1])
